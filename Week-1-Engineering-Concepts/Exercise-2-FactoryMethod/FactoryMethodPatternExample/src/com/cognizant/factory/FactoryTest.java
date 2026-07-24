9bc0369b294c3719d638db66e4929335a055d0dc
Abhinav Kumar <avinavluv4@gmail.com>
2026-07-25 02:24:39 +0530

    git commit -m "Complete Exercise 2: Implement Factory Method Pattern"

---
+++ Week-1-Engineering-Concepts/Exercise-2-FactoryMethod/FactoryMethodPatternExample/src/com/cognizant/factory/FactoryTest.java
@@ -0,0 +1,15 @@
+package com.cognizant.factory;
+
+public class FactoryTest {
+
+    public static void main(String[] args) {
+
+        DocumentFactory wordFactory = new WordDocumentFactory();
+        DocumentFactory pdfFactory = new PdfDocumentFactory();
+        DocumentFactory excelFactory = new ExcelDocumentFactory();
+
+        wordFactory.openDocument();
+        pdfFactory.openDocument();
+        excelFactory.openDocument();
+    }
+}
---
+++ Week-1-Engineering-Concepts/Exercise-2-FactoryMethod/README.md
@@ -0,0 +1,20 @@
+# Exercise 2 - Factory Method Pattern
+
+## Objective
+Implement the Factory Method Design Pattern to create different types of documents.
+
+## Classes
+- Document
+- WordDocument
+- PdfDocument
+- ExcelDocument
+- DocumentFactory
+- WordDocumentFactory
+- PdfDocumentFactory
+- ExcelDocumentFactory
+- FactoryTest
+
+## Output
+Opening Word Document...
+Opening PDF Document...
+Opening Excel Document...
