494aa16842f8a5d0f25da3ae4ef311ffd8c51599
Abhinav Kumar <avinavluv4@gmail.com>
2026-07-25 16:13:43 +0530

    Complete Exercise 8: Implement Strategy Pattern

---
+++ Week-1-Engineering-Concepts/Exercise-8-Strategy/README.md
@@ -0,0 +1,15 @@
+# Exercise 8 - Strategy Pattern
+
+## Objective
+Implement the Strategy Design Pattern to support multiple payment methods.
+
+## Classes
+- PaymentStrategy
+- CreditCardPayment
+- PayPalPayment
+- PaymentContext
+- StrategyTest
+
+## Output
+Paid $2500.0 using Credit Card.
+Paid $4500.0 using PayPal.
---
+++ Week-1-Engineering-Concepts/Exercise-8-Strategy/StrategyPatternExample/src/com/cognizant/strategy/StrategyTest.java
@@ -0,0 +1,15 @@
+package com.cognizant.strategy;
+
+public class StrategyTest {
+
+    public static void main(String[] args) {
+
+        PaymentContext context = new PaymentContext(new CreditCardPayment());
+
+        context.makePayment(2500);
+
+        context.setPaymentStrategy(new PayPalPayment());
+
+        context.makePayment(4500);
+    }
+}
