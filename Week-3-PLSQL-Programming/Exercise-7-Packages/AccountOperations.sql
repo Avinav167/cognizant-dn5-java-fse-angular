c06871bddc282309f4ea78df9a6686272c42b857
Abhinav Kumar <avinavluv4@gmail.com>
2026-07-26 14:42:00 +0530

    Complete Week 3 Exercise 7: Packages

---
+++ Week-3-PLSQL-Programming/Exercise-7-Packages/AccountOperations.sql
@@ -0,0 +1,102 @@
+/* Package Specification */
+
+CREATE OR REPLACE PACKAGE AccountOperations AS
+
+    PROCEDURE OpenAccount(
+
+        p_accountid NUMBER,
+        p_customerid NUMBER,
+        p_type VARCHAR2,
+        p_balance NUMBER
+
+    );
+
+    PROCEDURE CloseAccount(
+
+        p_accountid NUMBER
+
+    );
+
+    FUNCTION GetTotalBalance(
+
+        p_customerid NUMBER
+
+    ) RETURN NUMBER;
+
+END AccountOperations;
+/
+
+/* Package Body */
+
+CREATE OR REPLACE PACKAGE BODY AccountOperations AS
+
+    PROCEDURE OpenAccount(
+
+        p_accountid NUMBER,
+        p_customerid NUMBER,
+        p_type VARCHAR2,
+        p_balance NUMBER
+
+    )
+    IS
+    BEGIN
+
+        INSERT INTO Accounts(
+
+            AccountID,
+            CustomerID,
+            AccountType,
+            Balance,
+            LastModified
+
+        )
+
+        VALUES(
+
+            p_accountid,
+            p_customerid,
+            p_type,
+            p_balance,
+            SYSDATE
+
+        );
+
+    END;
+
+    PROCEDURE CloseAccount(
+
+        p_accountid NUMBER
+
+    )
+    IS
+    BEGIN
+
+        DELETE FROM Accounts
+        WHERE AccountID = p_accountid;
+
+    END;
+
+    FUNCTION GetTotalBalance(
+
+        p_customerid NUMBER
+
+    )
+    RETURN NUMBER
+
+    IS
+
+        v_total NUMBER;
+
+    BEGIN
+
+        SELECT SUM(Balance)
+        INTO v_total
+        FROM Accounts
+        WHERE CustomerID = p_customerid;
+
+        RETURN NVL(v_total,0);
+
+    END;
+
+END AccountOperations;
+/
---
+++ Week-3-PLSQL-Programming/Exercise-7-Packages/README.md
@@ -0,0 +1,20 @@
+# Exercise 7 - Packages
+
+## Scenarios
+
+1. CustomerManagement package
+2. EmployeeManagement package
+3. AccountOperations package
+
+## Concepts Used
+
+- Package Specification
+- Package Body
+- Procedures
+- Functions
+- SELECT INTO
+- INSERT
+- UPDATE
+- DELETE
+- SUM
+- NVL
