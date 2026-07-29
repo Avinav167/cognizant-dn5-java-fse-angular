b623d48f1471cd0f17411c00d4755aaf255e74d2
Abhinav Kumar <avinavluv4@gmail.com>
2026-07-29 20:43:07 +0530

    Completed Mockito Advanced Exercise 3: Mocking File I/O

---
+++ Week-4-JUnit-testing/Mockito-Advanced/Exercise-3-MockingFileIO/src/main/java/com/cognizant/FileService.java
@@ -0,0 +1,21 @@
+package com.cognizant;
+
+public class FileService {
+
+    private final FileReader fileReader;
+    private final FileWriter fileWriter;
+
+    public FileService(FileReader fileReader, FileWriter fileWriter) {
+        this.fileReader = fileReader;
+        this.fileWriter = fileWriter;
+    }
+
+    public String processFile() {
+
+        String data = fileReader.read();
+
+        fileWriter.write(data);
+
+        return "Processed " + data;
+    }
+}
